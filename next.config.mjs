3c755c40340c32a4049f23866ea990950c78f356
Vinícius Jardim <viniciuspjardim@gmail.com>
2023-04-01 18:34:53 -0300

    chore: remove internationalization config

--- next.config.mjs
+++ next.config.mjs
@@ -14,17 +14,6 @@ const config = {
     domains: ['images.clerk.dev'],
   },
 
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-
   experimental: { appDir: true },
 }
 export default config
