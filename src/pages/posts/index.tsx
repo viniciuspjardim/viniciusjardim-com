9a69c1015b8e06929b55c4e7366170c874ea3933
Vinícius Jardim <viniciuspjardim@gmail.com>
2023-04-08 17:38:08 -0300

    feat: display a warning on posts when not signed in

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,18 +1,36 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useUser } from '@clerk/nextjs'
 
 import { AuthButton } from '~/components/AuthButton'
 import { CreatePostForm } from '~/components/post/CreatePostForm'
 import { PostWithActions } from '~/components/post/PostWithActions'
 import { api } from '~/utils/api'
 
-export default function PostsPage() {
+export default function ManagePostsPage() {
   const { data, isLoading } = api.posts.getAll.useQuery()
+  const { user } = useUser()
+
+  if (!user) {
+    return (
+      <div className="flex flex-col items-center space-y-8 py-4">
+        <Link href={'/'}>Home</Link>
+
+        <AuthButton />
+
+        <div className="flex w-full max-w-3xl justify-center px-2">
+          <p className="rounded-r-md border-l-4 border-rose-500 bg-slate-900/75 p-4 text-base">
+            <strong>Info:</strong> Please sign in to create or edit posts!
+          </p>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
       <Head>
-        <title>Vinícius Jardim | Create a Post</title>
+        <title>Vinícius Jardim | Manage Post</title>
         <meta name="description" content="Vinícius Jardim's personal site" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
