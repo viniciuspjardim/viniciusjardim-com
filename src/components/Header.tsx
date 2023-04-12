def24215e2326da671aaa36fd9656cbce3dbf607
Vinícius Jardim <viniciuspjardim@gmail.com>
2023-04-11 21:09:15 -0300

    feat: create a basic header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { AuthButton } from '~/components/AuthButton'
+
+export function Header() {
+  return (
+    <header className="flex w-full max-w-3xl items-center justify-between gap-4 px-2">
+      <Link href={'/'}>Home</Link>
+
+      <nav className="gap4 flex items-center gap-4">
+        <Link href={'/posts'}>Manage Posts</Link>
+        <Link href={'/caches'}>Manage Caches</Link>
+        <AuthButton />
+      </nav>
+    </header>
+  )
+}
