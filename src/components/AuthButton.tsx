519159e53f6fbac05d272c7b641826d75d64768c
Vinícius Jardim <viniciuspjardim@gmail.com>
2023-05-19 18:10:08 -0300

    chore: update packages and fix Clerk breaking change
    The Clerk SignOutButton was removed.

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,22 +1,18 @@
 'use client'
 
-import { useUser, SignInButton, SignOutButton } from '@clerk/nextjs'
-
+import { useUser, useClerk, SignInButton } from '@clerk/nextjs'
 import { Button } from '~/components/Button'
 
 export function AuthButton() {
   const user = useUser()
+  const { signOut } = useClerk()
 
   if (!user.isLoaded) {
     return <Button disabled>Loading...</Button>
   }
 
   if (user.isSignedIn) {
-    return (
-      <SignOutButton>
-        <Button>Sign Out</Button>
-      </SignOutButton>
-    )
+    return <Button onClick={() => signOut()}>Sign Out</Button>
   }
 
   return (
