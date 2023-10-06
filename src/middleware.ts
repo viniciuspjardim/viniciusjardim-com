79617717bd600eec683d69eb63baff695172c61e
Vinícius Jardim <viniciuspjardim@gmail.com>
2023-10-06 20:18:13 -0300

    chore: update Clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,15 @@
-import { withClerkMiddleware } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
+import { authMiddleware } from '@clerk/nextjs'
 
-export default withClerkMiddleware(() => {
-  return NextResponse.next()
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/admin/posts',
+    '/admin/categories',
+    '/admin/caches',
+    '/api/trpc/(.*)',
+  ],
 })
 
-// Stop Middleware running on static files
 export const config = {
-  matcher:
-    '/((?!_next/image|_next/static|favicon.svg|favicon.ico|icon.svg|icon.png|color-beans-card.png|color-beans-icon.png).*)',
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 }
