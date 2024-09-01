0f89648069ad8e858951391b514310c3c368808a
Vinícius Jardim <viniciuspjardim@gmail.com>
2024-09-01 17:38:52 -0300

    feat: update Color Beans link style

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -12,13 +12,13 @@ export default async function HomePage() {
   return (
     <main className="mx-auto flex w-full max-w-6xl flex-col items-center px-4 md:px-10">
       <Link
-        className="my-12 flex items-center justify-center gap-4 rounded-xl border border-gray-900 px-8 py-4 text-xl"
+        className="my-12 flex w-full items-center justify-center gap-6 rounded-2xl border border-gray-900 bg-gray-950 px-8 py-4 text-xl transition-all hover:bg-gray-900/60 sm:w-auto"
         href="/p/color-beans"
       >
         <Image
-          src="/color-beans-icon.png"
-          width={36}
-          height={36}
+          src="/color-beans-2-icon.png"
+          width={42}
+          height={42}
           alt="Color Beans logo"
         />
         <span>Play Color Beans</span>
