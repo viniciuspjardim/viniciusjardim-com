5cd77f8174c5969c4fc1d8637e22e408c32a52b8
Vinícius Jardim <viniciuspjardim@gmail.com>
2024-09-07 09:14:17 -0300

    feat: update prism colors and add jsx and tsx support

--- src/components/post/json-parser.tsx
+++ src/components/post/json-parser.tsx
@@ -1,8 +1,10 @@
 import type { JSONContent } from '@tiptap/core'
 
 import Prism from 'prismjs'
-import 'prismjs/components/prism-typescript'
 import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-tsx'
 import 'prismjs/components/prism-json'
 import 'prismjs/components/prism-css'
 import 'prismjs/components/prism-scss'
