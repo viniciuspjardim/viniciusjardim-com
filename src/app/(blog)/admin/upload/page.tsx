14c045e9650f49a3c4e6a1573d1e46952d8717ac
Vinícius Jardim <viniciuspjardim@gmail.com>
2024-09-07 23:48:25 -0300

    feat: require login to access the upload admin page

--- src/app/(blog)/admin/upload/page.tsx
+++ src/app/(blog)/admin/upload/page.tsx
@@ -1,11 +1,27 @@
 'use client'
 
+import { useUser } from '@clerk/nextjs'
+
 import { WidthContainer } from '~/components/width-container'
 import { UploadButton } from '~/utils/uploadthing'
 
 const pageName = 'Upload'
 
 export default function UploadAdmin() {
+  const { user } = useUser()
+
+  if (!user) {
+    return (
+      <WidthContainer className="space-y-8 py-12">
+        <h1 className="text-3xl">{pageName}</h1>
+
+        <p className="rounded-r-md border-l-4 border-rose-600 bg-neutral-900 p-4 text-base">
+          <strong>Info:</strong> please sign in to access {pageName}.
+        </p>
+      </WidthContainer>
+    )
+  }
+
   return (
     <WidthContainer className="space-y-8 py-12">
       <h1 className="text-3xl">{pageName}</h1>
