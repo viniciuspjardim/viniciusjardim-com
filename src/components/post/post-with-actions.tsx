581d7267fb0052a0a2fe55c070e32d40649e4588
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-01-18 15:30:54 -0300

    feat: add alert dialog

--- src/components/post/post-with-actions.tsx
+++ src/components/post/post-with-actions.tsx
@@ -3,10 +3,24 @@ import type { JSONContent } from '@tiptap/core'
 import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Edit3Icon, TrashIcon } from 'lucide-react'
 
-import { JsonParser } from './json-parser'
 import { api } from '~/utils/api'
+import { JsonParser } from './json-parser'
 import { EditPostForm } from './edit-post-form'
+import { Button } from '~/components/ui/button'
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from '~/components/ui/alert-dialog'
+import { useToast } from '~/hooks/use-toast'
 
 type PostWithActionsProps = {
   id: number
@@ -34,8 +48,9 @@ export function PostWithActions({
   const ctx = api.useUtils()
 
   const [isEditing, setIsEditing] = useState(false)
+  const { toast } = useToast()
 
-  const { mutate: remove, isLoading: isRemovingPost } =
+  const { mutateAsync: remove, isLoading: isRemovingPost } =
     api.posts.remove.useMutation({
       onSuccess: async () => {
         await ctx.posts.getAll.invalidate()
@@ -61,7 +76,7 @@ export function PostWithActions({
 
   return (
     <article className="w-full space-y-4">
-      <div className="flex justify-between">
+      <div className="flex justify-between gap-3">
         <Link
           className="decoration-rose-800 hover:underline"
           href={`/posts/${slug}`}
@@ -71,22 +86,55 @@ export function PostWithActions({
           </h2>
         </Link>
 
-        <div className="flex justify-center gap-4">
-          <button
-            className="opacity-70 transition hover:opacity-100"
-            disabled={isRemovingPost}
-            onClick={() => setIsEditing(true)}
-          >
-            Edit
-          </button>
+        <div className="flex justify-center gap-3">
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button
+                className="px-2"
+                variant="destructive"
+                disabled={isRemovingPost}
+              >
+                <TrashIcon className="size-5" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This will permanently delete your{' '}
+                  <strong className="font-semibold">&quot;{title}&quot;</strong>{' '}
+                  post. This action cannot be undone.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  disabled={isRemovingPost}
+                  onClick={async () => {
+                    try {
+                      await remove({ id })
+                      toast({
+                        description: `Your post has been deleted.`,
+                      })
+                    } catch (error) {
+                      toast({
+                        variant: 'destructive',
+                        description:
+                          'An error occurred while deleting your post.',
+                      })
+                    }
+                  }}
+                >
+                  <TrashIcon className="mr-2 size-5" />
+                  <span>Delete</span>
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
 
-          <button
-            className="opacity-70 transition hover:opacity-100"
-            disabled={isRemovingPost}
-            onClick={() => remove({ id })}
-          >
-            Remove
-          </button>
+          <Button disabled={isRemovingPost} onClick={() => setIsEditing(true)}>
+            <Edit3Icon className="mr-2 size-5" /> <span>Edit</span>
+          </Button>
         </div>
       </div>
 
