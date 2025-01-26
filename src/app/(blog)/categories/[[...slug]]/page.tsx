2227a6801aa6de4e71171e417967314a9922f148
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-01-26 11:53:38 -0300

    feat: create a route for getting the posts filtered by the category

--- src/app/(blog)/categories/[[...slug]]/page.tsx
+++ src/app/(blog)/categories/[[...slug]]/page.tsx
@@ -1,5 +1,6 @@
 import 'server-only'
 
+import { InfoIcon } from 'lucide-react'
 import { PostCard } from '~/components/post/post-card'
 import { WidthContainer } from '~/components/width-container'
 import { api } from '~/trpc/server'
@@ -12,16 +13,15 @@ export default async function CategoriesPage({
   params: Promise<{ slug: string }>
 }) {
   const slug = (await params).slug?.[0]
-  const [categories, posts] = await Promise.all([
-    api.categories.getAllFlat.query(),
-    api.posts.getAll.query(),
-  ])
+  const { categories, posts } = await api.posts.getAllByCategorySlug.query({
+    categorySlug: slug,
+  })
 
   return (
     <WidthContainer className="flex w-full flex-col items-center">
       <div className="flex w-full flex-wrap gap-2 pt-16">
         <CategoryPill title="All" selectedSlug={slug} />
-        {categories?.map((category) => (
+        {categories.map((category) => (
           <CategoryPill
             key={category.id}
             selectedSlug={slug}
@@ -31,8 +31,8 @@ export default async function CategoriesPage({
         ))}
       </div>
 
-      <div className="my-16 divide-y divide-dashed divide-neutral-800">
-        {posts?.map((post) => (
+      <div className="my-16 w-full divide-y divide-dashed divide-neutral-800">
+        {posts.map((post) => (
           <PostCard
             key={post.id}
             slug={post.slug}
@@ -44,6 +44,15 @@ export default async function CategoriesPage({
             userImageUrl={post.author?.userImageUrl}
           />
         ))}
+
+        {posts.length === 0 && (
+          <div className="w-full rounded-md border border-neutral-800 py-24 text-center">
+            <InfoIcon className="mx-auto size-8 text-rose-800" />
+            <span className="mt-2 block text-lg text-neutral-400">
+              No posts here yet!
+            </span>
+          </div>
+        )}
       </div>
     </WidthContainer>
   )
--- src/server/api/routers/post-router.ts
+++ src/server/api/routers/post-router.ts
@@ -37,6 +37,51 @@ export const postRouter = createTRPCRouter({
     }))
   }),
 
+  // TODO: move this to it's own router like `categoryPageRouter`
+  getAllByCategorySlug: publicProcedure
+    .input(z.object({ categorySlug: z.string().min(1).max(200).optional() }))
+    .query(async ({ ctx, input }) => {
+      // Get all categories
+      const flatCategories = await ctx.db.category.findMany({
+        orderBy: [{ rank: 'desc' }, { createdAt: 'asc' }],
+      })
+
+      // Get category by slug
+      const category = input.categorySlug
+        ? flatCategories.find(
+            (category) => category.slug === input.categorySlug
+          )
+        : null
+
+      // If category slug is provided, but not found, throw an error
+      if (input.categorySlug && !category) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Category not found',
+        })
+      }
+
+      // Get all posts or posts by the category
+      // TODO: get post also from subcategories when the category is provided
+      const posts = await ctx.db.post.findMany({
+        where: { categoryId: category?.id },
+        orderBy: [{ rank: 'desc' }, { writtenAt: 'desc' }],
+      })
+
+      const users = (
+        await clerkClient.users.getUserList({
+          userId: posts.map((post) => post.authorId),
+        })
+      ).map(filterUserFields)
+
+      const postsWithAuthor = posts.map((post) => ({
+        ...post,
+        author: users.find((user) => user.id === post.authorId),
+      }))
+
+      return { categories: flatCategories, posts: postsWithAuthor }
+    }),
+
   getOne: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(async ({ ctx, input }) => {
