117b78b9e5b937cc40603005d5e8dc5a2a9f6b50
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-03-03 10:27:36 -0300

    chore: fix Tailwind autocomplete, hover preview and sorting on cva, clsx and cn function

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -7,7 +7,13 @@ const config = {
   useTabs: false,
   endOfLine: 'lf',
 
+  /*
+   * Prettier tailwindcss plugin config for sorting classes.
+   * See Tailwind CSS IntelliSense extension config in `.vscode/settings.json`
+   */
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tailwindAttributes: ['class', 'classes', 'className', 'classNames'],
+  tailwindFunctions: ['clsx', 'cva', 'cn'],
 }
 
 module.exports = config
