698c49afcc49899de176f0bfac1dceeed1e87c33
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-03-08 15:38:09 -0300

    feat: change text to speech character limit to 4096

--- src/server/api/routers/post-router.ts
+++ src/server/api/routers/post-router.ts
@@ -190,7 +190,7 @@ export const postRouter = createTRPCRouter({
           titlesSeparator: '.\n\n',
         })
 
-        const content = z.string().min(1).max(4000).parse(postText)
+        const content = z.string().min(1).max(4096).parse(postText)
         const speechBuffer = await generateSpeech(content)
         const [utFile] = await upload(speechBuffer, input.slug)
 
