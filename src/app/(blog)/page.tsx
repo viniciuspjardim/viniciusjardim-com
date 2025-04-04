f9ff16653c99d8e5d9ea39e5440610d4b3b39b38
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-04-04 15:05:15 -0300

    fix: remove unnecessary HydrateClient from the home page

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -3,28 +3,26 @@ import 'server-only'
 import { PostCard } from '~/components/post/post-card'
 import { WidthContainer } from '~/components/width-container'
 import { ColorBeans } from '~/components/projects/color-beans'
-import { api, HydrateClient } from '~/trpc/server'
+import { api } from '~/trpc/server'
 import { formatAuthorName } from '~/helpers/format-author-name'
 
 export default async function HomePage() {
   const posts = await api.posts.getAll()
 
   return (
-    <HydrateClient>
-      <WidthContainer className="flex w-full flex-col items-center">
-        <ColorBeans />
+    <WidthContainer className="flex w-full flex-col items-center">
+      <ColorBeans />
 
-        <div className="mb-16 divide-y divide-dashed divide-neutral-800">
-          {posts?.map((post) => (
-            <PostCard
-              key={post.id}
-              post={post}
-              userName={formatAuthorName(post.author)}
-              userImageUrl={post.author?.userImageUrl}
-            />
-          ))}
-        </div>
-      </WidthContainer>
-    </HydrateClient>
+      <div className="mb-16 divide-y divide-dashed divide-neutral-800">
+        {posts?.map((post) => (
+          <PostCard
+            key={post.id}
+            post={post}
+            userName={formatAuthorName(post.author)}
+            userImageUrl={post.author?.userImageUrl}
+          />
+        ))}
+      </div>
+    </WidthContainer>
   )
 }
