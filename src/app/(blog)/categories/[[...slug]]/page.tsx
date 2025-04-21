074a4390c6febd8710cc3049df97690922b3f0c4
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-04-20 21:53:27 -0300

    fix: category no post spacing

--- src/app/(blog)/categories/[[...slug]]/page.tsx
+++ src/app/(blog)/categories/[[...slug]]/page.tsx
@@ -25,7 +25,7 @@ export default async function CategoryPage({
       ))}
 
       {posts.length === 0 && (
-        <div className="w-full rounded-md border px-6 py-44 text-center">
+        <div className="mt-10 w-full rounded-md border px-6 py-40 text-center">
           <InfoIcon className="mx-auto size-8 text-rose-800" />
           <span className="mx-auto mt-2 block max-w-lg text-lg font-semibold text-balance text-neutral-300">
             Oops, no posts around here yet...
