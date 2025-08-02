70f43db8e54b275cefa632c1c6278519717f2e75
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-08-02 20:01:23 -0300

    chore: updated dependencies

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -3,6 +3,6 @@ import { UTApi, UTFile } from 'uploadthing/server'
 export const utApi = new UTApi()
 
 export async function upload(buffer: Buffer, slug: string) {
-  const file = new UTFile([buffer], `${slug}.mp3`)
+  const file = new UTFile([new Uint8Array(buffer)], `${slug}.mp3`)
   return utApi.uploadFiles([file])
 }
