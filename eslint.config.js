ede2f28ffca5f4f4b00ad418504974bf140fa94f
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-10-26 19:31:36 -0300

    chore: migrate ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,68 +1,21 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+// @ts-expect-error - no types for this plugin
+import nextVitals from 'eslint-config-next/core-web-vitals'
+// @ts-expect-error - no types for this plugin
+import nextTs from 'eslint-config-next/typescript'
 
-// @ts-ignore -- no types for this plugin
-import drizzle from 'eslint-plugin-drizzle'
-import reactCompiler from 'eslint-plugin-react-compiler'
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
-
-export default tseslint.config(
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
   {
-    ignores: ['.next'],
-  },
-  ...compat.extends('next/core-web-vitals'),
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      drizzle,
-      'react-compiler': reactCompiler,
-    },
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
     rules: {
-      'react-compiler/react-compiler': 'error',
-      'drizzle/enforce-delete-with-where': [
-        'error',
-        { drizzleObjectName: ['db', 'ctx.db'] },
-      ],
-      'drizzle/enforce-update-with-where': [
-        'error',
-        { drizzleObjectName: ['db', 'ctx.db'] },
-      ],
-      '@typescript-eslint/prefer-nullish-coalescing': 'off',
-      '@typescript-eslint/array-type': 'off',
-      '@typescript-eslint/consistent-type-definitions': 'off',
-      '@typescript-eslint/await-thenable': 'error',
-      '@typescript-eslint/require-await': 'off',
-      '@typescript-eslint/consistent-type-imports': [
-        'warn',
-        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
-      ],
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
       ],
-
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        { checksVoidReturn: { attributes: false } },
-      ],
     },
   },
-  {
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
-    },
-  }
-)
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
+
+export default eslintConfig
