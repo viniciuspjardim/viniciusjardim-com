b5be2a033fcfbd8e97f709d1e9271ee452617b44
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-11-23 20:29:41 -0300

    fix: remove no longer necessary @ts-expect-error on eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
-// @ts-expect-error - no types for this plugin
 import nextVitals from 'eslint-config-next/core-web-vitals'
-// @ts-expect-error - no types for this plugin
 import nextTs from 'eslint-config-next/typescript'
 
 // TODO: configure and test `eslint-plugin-drizzle``
