e2e7ec8e80d9b99270179b072e6a6834384135de
Vinícius Jardim <viniciuspjardim@gmail.com>
2025-12-25 17:04:10 -0300

    refactor: remove unused API endpoints

--- src/app/api/posts/get-all/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { db } from '~/db'
-
-export async function GET() {
-  const posts = await db.post.getAll()
-
-  return NextResponse.json(posts)
-}
--- src/app/api/system-info/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({
-    date: new Date().toISOString(),
-    bunVersion: Bun.version,
-  })
-}
